708d8e6e83e6825ab141c9d9e03907c86515a143
vatte vidhyadhar reddy <v.vidhyadharreddy9@gmail.com>
2026-03-09 14:56:52 +0530

    routers added

---
+++ src/routes/messageRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { getMessages } = require("../controllers/messageController");
+
+router.get("/:conversationId", getMessages);
+
+module.exports = router;
